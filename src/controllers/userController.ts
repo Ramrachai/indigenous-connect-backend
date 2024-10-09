445fb8cb1fc0a63eddea53e90293476fe249fae8
ramrachai <ramrachaim@gmail.com>
2024-10-09 12:47:04 +0600

    sort user : new user show first.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -16,10 +16,11 @@ export const getAllUsers = async (req: Request, res: Response) => {
         const limit = query.limit || 10;
         const search = query.search ? { fullname: new RegExp(query.search, "i") } : {};
 
-        const users = await User.find(search, "_id fullname email role status whatsapp ethnicity avatar") 
+        const users = await User.find(search, "_id fullname email role status whatsapp ethnicity avatar")
+            .sort({ createdAt: -1 })
             .skip((page - 1) * limit)
             .limit(limit)
-            .lean() 
+            .lean()
             .exec();
 
         const totalUsers = await User.countDocuments(search);
@@ -43,82 +44,82 @@ export const getAllUsers = async (req: Request, res: Response) => {
 
 
 export const updateUserStatus = async (req: Request, res: Response) => {
-  try {
-      const userId = req.params.id;
-      const { status } = updateUserStatusSchema.parse(req.body);
+    try {
+        const userId = req.params.id;
+        const { status } = updateUserStatusSchema.parse(req.body);
 
-      const user = await User.findByIdAndUpdate(userId, { status }, { new: true });
+        const user = await User.findByIdAndUpdate(userId, { status }, { new: true });
 
-      if (!user) return res.status(404).json({ error: "User not found" });
+        if (!user) return res.status(404).json({ error: "User not found" });
 
-      res.status(200).json({ message: "Status updated successfully", user });
-  } catch (error) {
-      if (error instanceof Error) {
-          res.status(400).json({ error: error.message });
-      } else {
-          res.status(400).json({ error: "An unknown error occurred" });
-      }
-  }
+        res.status(200).json({ message: "Status updated successfully", user });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(400).json({ error: "An unknown error occurred" });
+        }
+    }
 };
 
 
 
 export const updateUserRole = async (req: Request, res: Response) => {
-  try {
-      const userId = req.params.id;
-      const { role } = updateUserRoleSchema.parse(req.body);
+    try {
+        const userId = req.params.id;
+        const { role } = updateUserRoleSchema.parse(req.body);
 
-      const user = await User.findByIdAndUpdate(userId, { role }, { new: true });
+        const user = await User.findByIdAndUpdate(userId, { role }, { new: true });
 
-      if (!user) return res.status(404).json({ error: "User not found" });
+        if (!user) return res.status(404).json({ error: "User not found" });
 
-      res.status(200).json({ message: "Role updated successfully", user });
-  } catch (error) {
-      if (error instanceof Error) {
-          res.status(400).json({ error: error.message });
-      } else {
-          res.status(400).json({ error: "An unknown error occurred" });
-      }
-  }
+        res.status(200).json({ message: "Role updated successfully", user });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(400).json({ error: "An unknown error occurred" });
+        }
+    }
 };
 
 
 
 export const deleteUser = async (req: Request, res: Response) => {
-  try {
-      const userId = req.params.id;
+    try {
+        const userId = req.params.id;
 
-      const user = await User.findByIdAndDelete(userId);
+        const user = await User.findByIdAndDelete(userId);
 
-      if (!user) return res.status(404).json({ error: "User not found" });
+        if (!user) return res.status(404).json({ error: "User not found" });
 
-      res.status(200).json({ message: "User deleted successfully" });
-  } catch (error) {
-    if (error instanceof Error) {
-        res.status(400).json({ error: error.message });
-    } else {
-        res.status(400).json({ error: "An unknown error occurred" });
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(400).json({ error: "An unknown error occurred" });
+        }
     }
-  }
 };
 
 
 
 
 export const getUser = async (req: Request, res: Response) => {
-  try {
-      const userId = req.params.id;
+    try {
+        const userId = req.params.id;
 
-      const user = await User.findById(userId);
+        const user = await User.findById(userId);
 
-      if (!user) return res.status(404).json({ error: "User not found" });
+        if (!user) return res.status(404).json({ error: "User not found" });
 
-      res.status(200).json(user);
-  } catch (error) {
-      if (error instanceof Error) {
-          res.status(400).json({ error: error.message });
-      } else {
-          res.status(400).json({ error: "An unknown error occurred" });
-      }
-  }
+        res.status(200).json(user);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(400).json({ error: "An unknown error occurred" });
+        }
+    }
 };
